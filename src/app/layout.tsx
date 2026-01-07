ea0d6612254cdc6840bddcbd50dd72e388c68887
xhechar <felkozdzonchez3@gmail.com>
2026-01-07 14:25:08 +0300

    open next added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,6 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-export const runtime = "edge";
-
 export const twkLausanne = localFont({
   src: [
     {
